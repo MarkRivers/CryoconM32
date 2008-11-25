73d0cefc636b14f7ca059ba7272077adcd700f60
Linda Pratt <Linda.Pratt@diamond.ac.uk>
2008-11-25 14:24:58 +0000

    Changes to filenames in templates edl files, Makefiles etc for new CryoconM32 module.

--- example/exampleApp/opi/runGUI.sh
+++ example/exampleApp/opi/runGUI.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 . /home/tools/bin/changeports 6064
-EDMDATAFILES=.:/dls_sw/work/R3.14.8.2/support/CryoconM14/data
-#EDMDATAFILES=.:../../../CryoconM14App/opi/edl
+EDMDATAFILES=.:/dls_sw/work/R3.14.8.2/support/CryoconM32/data
+#EDMDATAFILES=.:../../../CryoconM32App/opi/edl
 export EDMDATAFILES
 edm -x -eolc -m "tmon=TS-ID-TMON-01,device=TS-ID-TMON-01,record1=T1,record2=T2,record3=T3,record4=T4" M32.edl &
 
