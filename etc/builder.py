e22f64abb5ca27ee9876baa3faf255e90b8ce317
Linda Pratt <Linda.Pratt@diamond.ac.uk>
2009-09-10 10:04:53 +0000

    CryoconM32: successful implementation of simulation IOC with IOC builder.

--- etc/builder.py
+++ etc/builder.py
@@ -2,21 +2,23 @@ from iocbuilder import Substitution
 from iocbuilder.arginfo import *
 from iocbuilder.modules.streamDevice import AutoProtocol
 from iocbuilder.modules.asyn import AsynOctetInterface
+from iocbuilder.modules.calc import Calc
 
-class M32_system(Substitution, AutoProtocol):
-    '''A brief description for this class goes here'''
+class CryoconM32(Substitution, AutoProtocol):
+    '''CryoconM32 temperature controller device class (includes sensors and control loops configured with other classes).'''
+    Dependencies = [Calc]
 
     # The __init__ method specifies arguments and defaults
-    def __init__(self, tctrlr, port, egu):
+    def __init__(self, tctrlr, port, egu="K"):
         # Filter the list of local variables by the argument list,
         # then initialise the super class
         self.__super.__init__(**filter_dict(locals(), self.Arguments))
 
     # __init__ arguments
     ArgInfo = makeArgInfo(__init__,
-        tctrlr = Simple('Description for tctrlr', str),
+        tctrlr = Simple('The PV prefix for the temperature controller', str),
         port   = Ident ('Asyn Port', AsynOctetInterface),
-        egu    = Simple('Description for egu', str))
+        egu    = Simple('Engineering units for sink temperature', str))
 
     # Substitution attributes
     TemplateFile = 'M32_system.template'
@@ -25,77 +27,73 @@ class M32_system(Substitution, AutoProtocol):
     # AutoProtocol attributes
     ProtocolFiles = ['CryoconM32.protocol']
 
-
-class M32_sensor(Substitution, AutoProtocol):
-    '''A brief description for this class goes here'''
+class CryoconM32_sensor(Substitution):
+    '''A sensor channel for the CryoconM32'''
 
     # The __init__ method specifies arguments and defaults
-    def __init__(self, record, device, sensor, port, egu, scan, adel, hihi, high, low, lolo, tctrlr, name = '', desc = '', gda = True):
+    def __init__(self, tctrlr, record, device, sensor, egu, adel, hihi, high, low, lolo, scan="2", name = '', desc = '', gda = True):
         # If gda then define gda_name and gda_desc
         if gda:
             gda_name, gda_desc = name, desc
         else:
             gda_name, gda_desc = '', ''
+        # Grab Asyn port and temperature controller prefix from "owning" CryoconM32 object.
+        port=tctrlr.args['port']
+        tctrlr = tctrlr.args['tctrlr']
         # Filter the list of local variables by the argument list,
         # then initialise the super class
         self.__super.__init__(**filter_dict(locals(), self.Arguments))
 
     # __init__ arguments
     ArgInfo = makeArgInfo(__init__,
-        record = Simple('Description for record', str),
-        device = Simple('Description for device', str),
-        sensor = Simple('Description for sensor', str),
-        port   = Ident ('Asyn Port', AsynOctetInterface),
-        egu    = Simple('Description for egu', str),
-        scan   = Simple('Description for scan', str),
-        adel   = Simple('Description for adel', str),
-        hihi   = Simple('Description for hihi', str),
-        high   = Simple('Description for high', str),
-        low    = Simple('Description for low', str),
-        lolo   = Simple('Description for lolo', str),
-        tctrlr = Simple('Description for tctrlr', str),
+        tctrlr = Ident('CryoconM32 object', CryoconM32),
+        record = Simple('Raw output suffix', str),
+        device = Simple('Sensor output prefix', str),
+        sensor = Choice('Sensor output suffix', ["A","B"]),
+        egu    = Simple('Engineering units for the sensor output', str),
+        adel   = Simple('Archive deadband increment', float),
+        hihi   = Simple('hihi alarm for sensor reading', float),
+        high   = Simple('high alarm for sensor reading', float),
+        low    = Simple('low alarm for sensor reading', float) ,
+        lolo   = Simple('lolo alarm for sensor reading', float),
+        scan   = Simple('Scan rate, e.g. 2 or .1', str),
         name   = Simple('Object name, also used for gda_name if gda', str),
         desc   = Simple('Object description, also used for gda_desc if gda', str),
         gda    = Simple('Set to True to make available to gda', bool))
 
     # Substitution attributes
     TemplateFile = 'M32_sensor.template'
-    Arguments = ArgInfo.Names()
+    Arguments = ArgInfo.Names() + ['port']
 
-    # AutoProtocol attributes
-    ProtocolFiles = ['CryoconM32.protocol']
-
-
-class M32_control(Substitution, AutoProtocol):
-    '''A brief description for this class goes here'''
+class CryoconM32_control(Substitution):
+    '''A control loop for the CryoconM32'''
 
     # The __init__ method specifies arguments and defaults
-    def __init__(self, tctrlr, loop, scan, port, rampratelim, name = '', desc = '', gda = True):
+    def __init__(self, tctrlr, loop, rampratelim, scan='2', name = '', desc = '', gda = True):
         # If gda then define gda_name and gda_desc
         if gda:
             gda_name, gda_desc = name, desc
         else:
             gda_name, gda_desc = '', ''
+
+        port=tctrlr.args['port']
+        tctrlr = tctrlr.args['tctrlr']
+
         # Filter the list of local variables by the argument list,
         # then initialise the super class
         self.__super.__init__(**filter_dict(locals(), self.Arguments))
 
     # __init__ arguments
     ArgInfo = makeArgInfo(__init__,
-        tctrlr      = Simple('Description for tctrlr', str),
-        loop        = Simple('Description for loop', str),
-        scan        = Simple('Description for scan', str),
-        port        = Ident ('Asyn Port', AsynOctetInterface),
-        rampratelim = Simple('Description for rampratelim', str),
+        tctrlr      = Ident('CryoconM32 object', CryoconM32),
+        loop        = Choice('Loop number', [1,2]),
+        rampratelim = Simple('Limit to the ramprate', float),
+        scan        = Simple('Scan rate', str),
         name        = Simple('Object name, also used for gda_name if gda', str),
         desc        = Simple('Object description, also used for gda_desc if gda', str),
         gda         = Simple('Set to True to make available to gda', bool))
 
     # Substitution attributes
     TemplateFile = 'M32_control.template'
-    Arguments = ArgInfo.Names()
-
-    # AutoProtocol attributes
-    ProtocolFiles = ['CryoconM32.protocol']
-
+    Arguments = ArgInfo.Names() + ['port']
 
